24253c23c48316bad06b68cf601823c069a6d57a
nlunscher <nlunscher@clearpath.ai>
2021-09-26 16:19:02 -0400

    did first step

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -14,6 +14,7 @@
 import cv2
 import numpy as np
 import torch
+import zlib
 
 # add project directory to python path to enable relative imports
 import os
@@ -59,18 +60,37 @@ def show_range_image(frame, lidar_name):
     print("student task ID_S1_EX1")
 
     # step 1 : extract lidar data and range image for the roof-mounted lidar
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0]
     
     # step 2 : extract the range and the intensity channel from the range image
+    if len(lidar.ri_return1.range_image_compressed) > 0:
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
+
+    ri_range = ri[:,:,0]
+    ri_intensity = ri[:,:,1]
     
+    # print(ri_range.shape)
+
     # step 3 : set values <0 to zero
-    
+    ri_range[ri_range<0]=0.0
+    ri_intensity[ri_intensity<0]=0.0
+
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
+    ri_range = ri_range * 255 / (np.amax(ri_range) - np.amin(ri_range)) 
     
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
-    
+    p1 = np.percentile(ri_intensity, 1.0)
+    p99 = np.percentile(ri_intensity, 99.0)
+
+    # print(f"p1 {p1} p99 {p99}")
+
+    ri_intensity = ri_intensity * 255 / (p99 - p1)
+
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
-    
-    img_range_intensity = [] # remove after implementing all steps
+    img_range_intensity = np.vstack((ri_range, ri_intensity)).astype(np.uint8)
+
     #######
     ####### ID_S1_EX1 END #######     
     
