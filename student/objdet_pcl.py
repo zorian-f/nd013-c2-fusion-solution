d2fe2a9b5bfa6aa49713d619a3ac988300561270
zorian-f <79806814+zorian-f@users.noreply.github.com>
2022-02-23 22:03:43 +0100

    Update objdet_pcl.py

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -147,20 +147,20 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX2")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
-    intensity_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
+    intensity_map = np.zeros((configs.bev_height, configs.bev_width))
     # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
     idx_intensity = np.lexsort((-lidar_pcl_cpy[:, 3], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
     lidar_pcl_cpy = lidar_pcl_cpy[idx_intensity]
     ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
     ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
-    counts, indices = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True)
+    lidar_pcl_int, indices, counts = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
     lidar_pcl_top = lidar_pcl_cpy[indices]
     ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
     ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
     ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
     
-    perecentile99 = np.percentile(lidar_pcl_top[:, 3], 99.96)
-    perecentile1 = np.percentile(lidar_pcl_top[:, 3], 0.8)
+    perecentile99 = np.percentile(lidar_pcl_top[:, 3], 99)
+    perecentile1 = np.percentile(lidar_pcl_top[:, 3], 1)
 
     idx_limit = lidar_pcl_top[:, 3] > perecentile99
     lidar_pcl_top[idx_limit, 3] = perecentile99
@@ -168,9 +168,12 @@ def bev_from_pcl(lidar_pcl, configs):
     idx_limit = lidar_pcl_top[:, 3] < perecentile1
     lidar_pcl_top[idx_limit, 3] = perecentile1
 
-    lidar_pcl_top[:,3] -= perecentile1
-    lidar_pcl_top[:,3] *= 255 / (perecentile99 - perecentile1)
-    intensity_map[np.int_(lidar_pcl_top[:,0]), np.int_(lidar_pcl_top[:,1])] = lidar_pcl_top[:,3]
+    #lidar_pcl_top[:,3] -= perecentile1
+    #lidar_pcl_top[:,3] *= 255 / (perecentile99 - perecentile1)
+
+    intensity_map[np.int_(lidar_pcl_top[:,0]), np.int_(lidar_pcl_top[:,1])] = lidar_pcl_top[:,3] / float(perecentile99 - perecentile1)
+    #intensity_map[np.int_(lidar_pcl_top[:,0]), np.int_(lidar_pcl_top[:,1])] = lidar_pcl_top[:,3] / (np.amax(lidar_pcl_top[:, 3])-np.amin(lidar_pcl_top[:, 3]))
+
     #intensity_map[np.int_(lidar_pcl_int[:, 0]), np.int_(lidar_pcl_int[:, 1])]
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
     #cv2.imshow('intensity_map', int_map)
